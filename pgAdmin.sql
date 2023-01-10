b18f0218d6469b4f7cc956166880fbffaf5fa70a
ras52017 <ras52017@yahoo.co.uk>
2023-01-10 01:12:06 -0600

    add files

---
+++ pgAdmin.sql
@@ -0,0 +1,8 @@
+SELECT * FROM public.customers_table;
+SELECT * FROM public.vine_table;
+SELECT * FROM public.review_id_table;
+SELECT * FROM public.products_table;
+
+select *
+FROM public.vine_table
+WHERE "total_votes">=20
